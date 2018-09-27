4a953dd73a0bf193cdd19c11d5a3f752cc736fcc
HerianEduward <43520686+HerianEduward@users.noreply.github.com>
2018-09-27 23:12:22 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,31 @@
-<?php
+<!DOCTYPE html>
+<head>
+  <title>Form Login</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-
-?>
+ <center><h2>LOGIN</h2>  
+  
+  <div class="login">
+  <br/>
+    <form action="TA.php" method="post" onSubmit="return validasi()">
+      <div>
+        <label>Username:</label>
+        <br/>
+        <input type="text" name="username">
+        <br/>
+      </div>
+      <div>
+        <label>Password:</label>
+        <br/>
+        <input type="password" name="password">
+        <br/>
+      </div>     
+      <div>
+        <input type="submit" value="Login" class="tombol">
+      </div>
+    </form>
+  </div>
+</body>
+ </CENTER>
+</html>
